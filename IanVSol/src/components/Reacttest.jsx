9b5e4f7065cc0c8126552ed81076c314cc26ef43
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-10-14 12:11:26 -0600

    adding another files

--- IanVSol/src/components/Reacttest.jsx
+++ IanVSol/src/components/Reacttest.jsx
@@ -4,10 +4,13 @@ export default function Counter({initialCount}) {
     const [count, setCount] = useState(initialCount)
     
     return (
-        <div>
+        <div className='text-white'>
             <h1>React Counter</h1>
-            <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <div className='flex gap-1 '>
+                <button className='bg-amber-200 text-black px-1 rounded-full hover:bg-red-500 hover:scale-105 transition-transform' onClick={() => setCount(count - 1)}>-</button>
+                <p>Count: {count}</p>
+                <button className='bg-amber-200 text-black px-1 rounded-full hover:bg-green-400 hover:scale-105 transition-transform' onClick={() => setCount(count + 1)}>+</button>
+            </div>
         </div>
     )
 }
