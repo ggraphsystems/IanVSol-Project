4844f0d112183d2f2a87f0f27bc3205ba032c28b
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-11-08 22:30:29 -0600

    the form services is almost done

---
+++ IanVSol/src/components/ModalCalendar.jsx
@@ -0,0 +1,21 @@
+import {X}  from 'feather-icons-react'
+
+export default function ModalCalendar({open, onClose, children}) {
+    return(
+        <div onClick={onClose} 
+        className={`fixed inset-0 flex justify-center items-center transition-color 
+        ${open ? "visible bg-black/20": "invisible"}`}>
+            <div 
+            onClick={(e) => e.stopPropagation()}
+            className={`bg-neutral-950 rounded-xl shadow p-2 py-15 lg:p-8 lg:py-8 transition-all duration-300 ease-in-out
+            ${open ? "scale-100 opacity-100" : "scale-90 opacity-0"}`}>
+                <button 
+                onClick={onClose}
+                className="absolute top-2 right-2 p-1 rounded-lg text-white hover:text-white cursor-pointer">
+                    <X/>
+                </button>
+                {children}
+            </div>
+        </div>
+    )
+}
--- IanVSol/src/components/js/Services.js
+++
@@ -1,9 +0,0 @@
-const production = document.getElementById("production");
-
-export default function Services() {
-    production.addEventListener("click", () => {
-    // e.preventDefault();
-    console.log("production");
-    });
-}
-Services();
