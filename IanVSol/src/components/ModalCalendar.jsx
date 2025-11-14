fdbc8c4377227ecc098f2580a4ab1221fa4cc10c
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-11-13 20:55:18 -0600

    making sure that the page is responsive

--- IanVSol/src/components/ModalCalendar.jsx
+++ IanVSol/src/components/ModalCalendar.jsx
@@ -1,4 +1,4 @@
-import {X}  from 'feather-icons-react'
+import {Check, X}  from 'feather-icons-react'
 
 export default function ModalCalendar({open, onClose, children}) {
     return(
@@ -7,12 +7,17 @@ export default function ModalCalendar({open, onClose, children}) {
         ${open ? "visible bg-black/20": "invisible"}`}>
             <div 
             onClick={(e) => e.stopPropagation()}
-            className={`bg-neutral-950 rounded-xl shadow p-2 py-15 lg:p-8 lg:py-8 transition-all duration-300 ease-in-out
+            className={`bg-neutral-950 rounded-xl shadow p-2 py-8 lg:p-8 lg:py-8 transition-all duration-300 ease-in-out
             ${open ? "scale-100 opacity-100" : "scale-90 opacity-0"}`}>
                 <button 
                 onClick={onClose}
                 className="absolute top-2 right-2 p-1 rounded-lg text-white hover:text-white cursor-pointer">
                     <X/>
+                </button>
+                <button 
+                onClick={onClose}
+                className="absolute top-105 right-2 p-1 rounded-lg text-white hover:text-white cursor-pointer">
+                    <Check/>
                 </button>
                 {children}
             </div>
