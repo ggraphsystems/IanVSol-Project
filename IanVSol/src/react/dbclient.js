0267ef1bf51e2bab036bed16c27d79f56ccb3977
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-11-26 22:47:23 -0600

    still fixing

---
+++ IanVSol/src/react/dbclient.js
@@ -0,0 +1,6 @@
+const api = {
+    supabaseUrl:"https://wvstghnnpoinpfpgseex.supabase.co",
+    supabaseKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2c3RnaG5ucG9pbnBmcGdzZWV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwODg4NDgsImV4cCI6MjA3OTY2NDg0OH0.PduYwUyVFr7T0vaIPHLvuuuuAu6i0b5gTz0CUPNcGnM"
+}
+
+export default api
