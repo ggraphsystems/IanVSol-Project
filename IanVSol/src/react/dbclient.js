0ebe800a4766fe5cd26fd5fbbaf06422facbdd1d
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-11-26 22:51:19 -0600

    still fixing

--- IanVSol/src/react/dbclient.js
+++ IanVSol/src/react/dbclient.js
@@ -1,3 +1,5 @@
+// service api
+
 const api = {
     supabaseUrl:"https://wvstghnnpoinpfpgseex.supabase.co",
     supabaseKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2c3RnaG5ucG9pbnBmcGdzZWV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwODg4NDgsImV4cCI6MjA3OTY2NDg0OH0.PduYwUyVFr7T0vaIPHLvuuuuAu6i0b5gTz0CUPNcGnM"
